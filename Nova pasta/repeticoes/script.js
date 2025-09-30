d6d7cc0f71f5ea8f2bd8f165283ebd27ad38b6ec
junoir2003 <andrejunoir0@gmail.com>
2025-09-29 22:16:09 -0300

    while

---
+++ Nova pasta/repeticoes/script.js
@@ -0,0 +1,14 @@
+/*let acertos = Number(prompt("quantos acertos?: "))
+let valorPorQuestao = 10/16
+let nota = acertos * valorPorQuestao
+
+alert("Sua nota foi " + nota)*/
+
+let contador = 0
+while(contador < 2){
+    contador = contador + 1
+    console.log("Contador: " + contador)
+}
+
+
+
