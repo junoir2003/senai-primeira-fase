cef32b58b0be15f38fdb9711eda217a541b1d1ff
junoir2003 <andrejunoir0@gmail.com>
2025-09-30 22:12:18 -0300

    for

---
+++ Nova pasta/aula-JavaScript/logica js/for.js
@@ -0,0 +1,32 @@
+// for (let i = 0; i <=10; i++){
+//     console.log(i)
+// }
+
+//  for (let i = 10; i >=0; i--){
+//      console.log(i)
+// }
+
+//  for (let i = 0; i <=20; i+=2){
+//      console.log(i)
+// }
+
+// let soma = 0
+
+// for (let i = 1; i<100; i++){
+//     soma += i
+// }console.log("soma:",soma)
+
+// let alunos = ["joao", "maria", "joana"]
+// alunos.push("pedro")
+
+// for(let i = 0; i<alunos.length; i++){
+//     console.log("alunos: ", alunos[i])
+// }
+//console.log(alunos.length)
+
+let frutas = ["laranja", "maça", "melancia", "Kiwi", "lichia"]
+//frutas.push("Banana")
+
+for(let i = 0; i<frutas.length; i++){
+    console.log("frutas: ", frutas[i])
+}
---
+++ Nova pasta/aula-JavaScript/logica js/funcoes.js
@@ -0,0 +1,24 @@
+// function saudacao (nome){
+// console.log(`Bom dia ${nome}`)
+// }
+
+// saudacao("André")
+
+// const saudacao = (nome)=>{
+//     console.log(`Bom dia ${nome}`)
+// }
+// saudacao("andré")
+// saudacao("junior")
+
+const soma = (x,y)=> x+y
+console.log(`soma: ${soma(10,5)}`)
+
+const multipliacao = (x,y)=> x*y
+console.log(`soma: ${multipliacao(10,5)}`)
+
+const divisao = (x,y)=> x/y
+console.log(`soma: ${divisao(10,5)}`)
+
+const substacao = (x,y)=> x-y
+console.log(`soma: ${substacao(10,5)}`)
+
